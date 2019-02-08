a379d799f80cc5e5b045a78edd3fc3f0a27ebb34
mostafa3 <mostafa_jcb@yahoo.com>
2019-02-08 20:36:01 +0200

    update sidebar

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -3,5 +3,9 @@
     <a class="nav-link disabled side-title"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a>
 
       <a class="nav-link side-link"  href="{{action('InventoryController@index')}}"><span>Inventories</span></a>
-      
+      <a class="nav-link side-link"  href="{{action('InventoryManagerController@index')}}"><span>Inventory Managers</span></a>
+
+    <a class="nav-link side-link"  href="{{action('SupplierController@index')}}"><span>Suppliers</span></a>
+    <a class="nav-link side-link"  href="{{action('ItemController@index')}}"><span>Items</span></a>
+    <a class="nav-link side-link"  href="{{action('TransactionController@index')}}"><span>Transactions</span></a>
   </div>
