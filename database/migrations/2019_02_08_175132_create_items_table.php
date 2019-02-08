f931ad0ec17e9c5ce6abb1169f9eb7f2b24bbdf1
mostafa3 <mostafa_jcb@yahoo.com>
2019-02-08 19:52:34 +0200

    migrate items

---
+++ database/migrations/2019_02_08_175132_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('name');
+          $table->string('unit');
+          $table->unsignedInteger('supplier_id');
+          $table->timestamps();
+
+          $table->foreign('supplier_id')->references('id')->on('suppliers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
