8f1c084cf30f2283ad81e5bbfa72839ab3b65aa6
mostafa3 <mostafa_jcb@yahoo.com>
2019-02-08 20:04:34 +0200

    migrate transactions

---
+++ database/migrations/2019_02_08_180225_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->string('amount'); // search
+            $table->unsignedInteger('item_id');
+            $table->mediumText('notes')->nullable();
+            $table->timestamps();
+
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
